c28f621f783749a77a181215fca92582c937491b
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-04-17 10:47:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ const preview = document.getElementById('preview');
 
 editor.addEventListener("input", () => {
     const value = editor.value;
-    preview.innerText = value;
+
+    let html = value;
+
+    html = html.replace(/^# (.*$)/gim, "<h1>$1</h1>");
+
+    preview.innerHTML = html;
 });
