75861f4f05a9197e15d8665c3d936c1872dbeec3
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-04-17 10:56:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ editor.addEventListener("input", () => {
     let html = value;
 
     html = html.replace(/^# (.*$)/gim, "<h1>$1</h1>");
+    html = html.replace(/^## (.*$)/gim, "<h2>$1</h2>");
+    html = html.replace(/^### (.*$)/gim, "<h3>$1</h3>");
 
+    html = html.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
+    html = html.replace(/\*(.*?)\*/g, "<em>$1</em>");
+
+    html = html.split(/\n\n+/).map(paragraph => `<p>${paragraph}</p>`).join("");
+
+    html = html.replace(/<p><\/p>/g, "");
     preview.innerHTML = html;
 });
