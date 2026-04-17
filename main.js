2a1b627135301ccef919447fb522290f1b0e2839
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-04-17 10:45:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const editor = document.getElementById('editor');
+const preview = document.getElementById('preview');
 
+editor.addEventListener("input", () => {
+    const value = editor.value;
+    preview.innerText = value;
+});
